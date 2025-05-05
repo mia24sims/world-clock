637b26d0366540f7c5e6c708c09e71fa4f50bd3a
mia24sims <167013397+mia24sims@users.noreply.github.com>
2025-05-04 23:34:53 -0500

    added functionality
    added id to select and to all cities
    add js function

--- javascript/index.jsx
+++ javascript/index.jsx
@@ -21,3 +21,22 @@ function udpateTime() {
 }
 udpateTime();
 setInterval(udpateTime, 1000);
+
+function updateCity(event) {
+  let cityTimeZone = event.target.value;
+  let cityName = cityTimeZone.replace("_", " ").split(`/`)[1];
+  let cityTime = moment().tz(cityTimeZone);
+  let citiesElement = document.querySelector("#cities");
+  citiesElement.innerHTML = `  <div class="city" >
+          <div>
+            <h2>${cityName}</h2>
+            <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
+          </div>
+          <div class="time">${cityTime.format(
+            "h:mm:ss"
+          )}<small>${cityTime.format("A")}</small></div>
+        </div>`;
+}
+
+let citiesSelect = document.querySelector("#city");
+citiesSelect.addEventListener("change", updateCity);
